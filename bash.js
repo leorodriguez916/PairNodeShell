f78d91b537ebc6a3da8cd9fca4f6863bf72d9a61
B0xGhost <jayjocasio@gmail.com>
2022-03-04 12:55:07 -0500

    fixed pwd problem

--- bash.js
+++ bash.js
@@ -1,11 +1,11 @@
-var filePath = require("pwd");
+// var filePath = require("pwd");
 
 process.stdout.write("prompt > ");
 
 process.stdin.on("data", (data) => {
   const cmd = data.toString().trim();
 
-  if (cmd == "pwd") process.stdout.write(filePath);
+  if (cmd == "pwd") process.stdout.write(process.cwd());
   else {
     process.stdout.write("You typed: " + cmd);
     process.stdout.write("\nprompt > ");
